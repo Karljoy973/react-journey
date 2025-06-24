803e26ee86a89072f0cec44d10a40d1dfa434537
Karl <karljoyeux@gmail.com>
2025-06-24 02:08:15 +0200

    j'ai presque fini, je vais faire ressortir le onCheck pour le mettre dans le parent ainsi je vais pouvoir chainer les opérateurs ternaires et chainer les .filter

---
+++ notes.md
@@ -0,0 +1,50 @@
+function App() {
+const [count, setCount] = useState(0);
+/**
+_ // pour muter des objects
+_ const [data, setData] = useState({key1: value1, key2: value2 ... });
+_ const handleEvent = () => {
+_ setData(...data, keyK: data.keyK: newValueK) parce qu'on ne peut pas faire de mutation dans un setter, on écrase la valeur
+_ attention je ne peux pas mettre un hook dans une condition, ou qu'il y ait de return avant
+_ je veux hook(function ) et non function(hook) \* \*/
+const handleClick = () => {
+setCount((count) => count +1);
+};
+/**
+_ attention aux input :
+_ Avec react il y a deux types d'input : les input contrôlés par react et ceux qui ne le sont pas (les input html classiques)
+_ input contrôlés doivent avoir une valeur et un handler : <input type="type" value={value} onChange={handleChange}/>
+_ Attention : value ne peut pas être null ou undefined
+_ input non-contrôlés ne doivent avoir ni valeur ni handler (on peut mettre une valeur par défaut defaultValue="value"
+_ Si on veut quand meme accéder à la valeur d'un input non contrôlé, on le met à l'intérieur d'un form sur lequel on place
+_ un handler.
+_ _/
+/\*\*
+_ Data Flow :
+_ normal Data Flow : parent -> enfant via props
+_ reverse Data Flow : enfant -> parent via props \*
+_ ex : voir setIsChecked
+_ \*/
+
+    const [firstname, setFirstname] = useState('');
+    const handleChange = (e) => {
+    	setFirstname(e.target.value);
+    }
+
+    const [isChecked, setIsChecked] = useState(false);
+
+return <>
+
+<p> Compteur {count} </p>
+<button onClick={handleClick} >Incrémenter</button>
+<form>
+<input type="text" value={firstname} onChange = {handleChange}/>
+<CGUCheckbox checked={isChecked} onCheck={setIsChecked}/>
+<button disabled={!isChecked}> Valider ! </button>
+</form>
+</>
+}
+
+const CGUCheckbox = ({checked, onCheck}) => {
+return <input type="checkbox" onChange={e => onCheck(e.target.checked)} checked={checked} />
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,147 @@
-import { useState } from 'react'
+import { useState } from "react";
+import './assets/main.css';
+type ProductCategoryProps = Pick<Product, "categorie">;
+type ProductRowProps = Pick<Product, "nom" | "prix">;
+const CategoryOptions = [
+  "Fruits",
+  "Légumes",
+  "Boissons",
+  "Épicerie",
+  "Produits laitiers",
+  "Boulangerie",
+] as const;
+type Product = {
+  category: (typeof CategoryOptions)[number];
+  prix: number;
+  nom: string;
+  stock: boolean;
+};
 
-const showTitle = false;
-const todos = ["cuisine", "nettoie ta chambre", "va faire les courses"];
+const PRODUCTS: Product[] = [
+  { categorie: "Fruits", prix: 3, nom: "Mangue", stock: true },
+  { categorie: "Fruits", prix: 5, nom: "Maracuja", stock: true },
+  { categorie: "Légumes", prix: 2, nom: "Carotte", stock: true },
+  { categorie: "Légumes", prix: 1.5, nom: "Concombre", stock: false },
+  { categorie: "Boissons", prix: 4, nom: "Jus d'orange", stock: true },
+  { categorie: "Épicerie", prix: 2.5, nom: "Riz basmati", stock: true },
+  { categorie: "Épicerie", prix: 6, nom: "Huile d'olive", stock: false },
+  {
+    categorie: "Produits laitiers",
+    prix: 3.2,
+    nom: "Lait entier",
+    stock: true,
+  },
+  {
+    categorie: "Produits laitiers",
+    prix: 2.8,
+    nom: "Yaourt nature",
+    stock: true,
+  },
+  { categorie: "Boulangerie", prix: 1.2, nom: "Baguette", stock: true },
+];
+
 function App() {
-	const [count, setCount] = useState(0); 
-	/**
-	* // pour muter des objects 
-	* const [data, setData] = useState({key1: value1, key2: value2 ... }); 
-	* const handleEvent = () => {
-	* setData(...data, keyK: data.keyK: newValueK)  parce qu'on ne peut pas faire de mutation dans un setter, on écrase la valeur
-	* attention je ne peux pas mettre un hook dans une condition, ou qu'il y ait de return avant 
-	* je veux hook(function ) et non function(hook) 
-	* */
-	const handleClick = () => {
-		setCount((count) => count +1); 
-	};
-  return <>
-		<p> Compteur {count} </p>
-	
-	<button onClick={handleClick} >Incrémenter</button>
-	</>
+  const [onCheck, setOnCheck] = useState(false);
+  const handleOnCheck = (e) => setOnCheck(e);
+
+  return (
+    <>
+      <SearchBar checked={onCheck} handleOnCheck={handleOnCheck} />
+      {onCheck ? (
+        <ProductTable products={PRODUCTS.filter((product) => product.stock)} />
+      ) : (
+        <ProductTable products={PRODUCTS} />
+      )}
+    </>
+  );
+}
+
+function SearchBar({ checked, handleOnCheck }) {
+  const [onFilled, setOnFilled] = useState("");
+  const handleOnFilled = (e) => setOnField(e);
+
+  return (
+    <>
+      <ProductSearch filled={onFilled} onFilled={setOnFilled} />
+      <Checkbox checked={checked} onCheck={(e) => handleOnCheck(e)} />
+    </>
+  );
+}
+
+function ProductSearch({ filled, onFilled }) {
+  return (
+    <>
+      <input
+	className='recherche-produit'
+        type="search"
+        value={filled}
+        onChange={(e) => onFilled(e.target.value)}
+      />{" "}
+    </>
+  );
+}
+function Checkbox({ checked, onCheck }) {
+  return (
+    <>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => onCheck(e.target.checked)}
+      />
+      <p>N'afficher que les produits en stocks ? </p>
+    </>
+  );
+}
+
+function ProductTable({ products }: ProductRowProps[]) {
+  return (
+    <>
+
+      <table><thead>
+        <tr>
+          <th scope="col">Nom du Produit</th>
+          <th scope="col">Prix</th>
+        </tr>
+      </thead>
+      <tbody>
+        {CategoryOptions.map((_categorie, index) => (
+          <ProductCategory key={index} categorie={_categorie} products={products} />
+        ))}
+      </tbody></table>
+
+    </>
+  );
+}
+function ProductCategory({ categorie, products }) {
+  return (
+    <>
+      <ProductCategoryRow categorie={categorie} />
+      {products
+        .filter((product) => product.categorie == categorie)
+        .map((product, index) => (
+          <ProductRow key={index} nom={product.nom} prix={product.prix} />
+        ))}
+    </>
+  );
+}
+
+function ProductCategoryRow({ categorie }: ProductCategoryProps) {
+  return (
+    <>
+      <tr className='categorie-produit'><td>{categorie}</td></tr>
+    </>
+  );
+}
+
+function ProductRow({ nom, prix }: ProductRowProps) {
+  return (
+    <>
+      <tr className='produit'>
+        <td>{nom}</td>
+        <td>{prix} €</td>
+      </tr>
+    </>
+  );
 }
 
 export default App;
-
