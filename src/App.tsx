f49ae1767c07cdab731f684724057da4c723e75f
Karl <karljoyeux@gmail.com>
2025-06-27 19:00:07 +0200

    studying how to use lifecycle hooks with react

--- src/App.tsx
+++ src/App.tsx
@@ -1,175 +1,70 @@
-import { useState } from "react";
-import "./assets/main.css";
-type ProductCategoryProps = Pick<Product, "categorie">;
-type ProductRowProps = Pick<Product, "nom" | "prix">;
-const CategoryOptions = [
-  "Fruits",
-  "Légumes",
-  "Boissons",
-  "Épicerie",
-  "Produits laitiers",
-  "Boulangerie",
-] as const;
-type Product = {
-  category: (typeof CategoryOptions)[number];
-  prix: number;
-  nom: string;
-  stock: boolean;
-};
-
-const PRODUCTS: Product[] = [
-  { categorie: "Fruits", prix: 3, nom: "Mangue", stock: true },
-  { categorie: "Fruits", prix: 5, nom: "Maracuja", stock: true },
-  { categorie: "Légumes", prix: 2, nom: "Carotte", stock: true },
-  { categorie: "Légumes", prix: 1.5, nom: "Concombre", stock: false },
-  { categorie: "Boissons", prix: 4, nom: "Jus d'orange", stock: true },
-  { categorie: "Épicerie", prix: 2.5, nom: "Riz basmati", stock: true },
-  { categorie: "Épicerie", prix: 6, nom: "Huile d'olive", stock: false },
-  {
-    categorie: "Produits laitiers",
-    prix: 3.2,
-    nom: "Lait entier",
-    stock: true,
-  },
-  {
-    categorie: "Produits laitiers",
-    prix: 2.8,
-    nom: "Yaourt nature",
-    stock: true,
-  },
-  { categorie: "Boulangerie", prix: 1.2, nom: "Baguette", stock: true },
-];
+import { useState, useEffect } from "react";
 
 function App() {
-  const [onCheck, setOnCheck] = useState(false);
-  const handleOnCheck = (e) => setOnCheck(e);
-  const [onFilled, setOnFilled] = useState("");
-  const handleOnFilled = (e) => setOnFilled(e);
+  const [title, setTitle] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [timer, setTimer] = useState(5);
+  let timeHandlerInterval = null;
+  const timeHandler = (value: number) => {
+    if (!!timeHandlerInterval) {
+      clearInterval(timeHandlerInterval);
+      timeHandlerInterval = null;
+    }
+    setTimer((timer) => (timer = value));
+    timeHandlerInterval = setInterval(() => {
+      setTimer((timer) => {
+        if (timer > 0) timer--;
+        else timer = 0;
+      });
+    }, 1000);
+  };
+  // pourquoi utiliser useEffect ?
+  // useEffect permet de créer des méthode de cycle de vie dans les composants fonctionnels
+  // donc quand il y a une mutation dans le useEffect, la mutation n'affecte pas tout le composant
+  // et ya que ce qui change qui est re-rendu => gain de performances.
+  // on peut aussi appeler une fonction de 'nettoyage'
+  // ex : useEffect(() => {
+  // window.addEventListener('event', e => handler);
+  // ...
+  // return () => window.removeEventListener('event', handler) } , []}
+  //
+  // On évite les setState dans le useEffect, dans la fonction de nettoyage pk pas
+  useEffect(() => {
+    const originalTitle = document.title;
+    document.title = title;
+    return () => (document.title = originalTitle);
+  }, [title]);
+
+  useEffect(() => {
+    const timerText = document.getElementById("text-timer") as HTMLElement;
+    const currentText = timerText.innerText;
+    timerText.innerText = `Il vous reste ${timer} s`;
+    return () => (timerText.innerText = currentText);
+  }, [timer]);
 
   return (
-    <>
-      <SearchBar
-        checked={onCheck}
-        handleOnCheck={handleOnCheck}
-        onFilled={onFilled}
-        handleOnFilled={handleOnFilled}
-      />
-      {onCheck && !!onFilled ? (
-        <ProductTable
-          products={PRODUCTS.filter((product) => product.stock).filter(
-            (product) => product.nom.includes(onFilled),
-          )}
-        />
-      ) : onCheck && !onFilled ? (
-        <ProductTable products={PRODUCTS.filter((product) => product.stock)} />
-      ) : !onCheck && onFilled ? (
-        <ProductTable
-          products={PRODUCTS.filter((product) =>
-            product.nom.includes(onFilled),
-          )}
-        />
-      ) : (
-        <ProductTable products={PRODUCTS} />
-      )}
-      <p>{onFilled}</p>
-    </>
-  );
-}
-
-function SearchBar({ checked, handleOnCheck, filled, handleOnFilled }) {
-  return (
-    <>
-      <ProductSearch
-        filled={filled}
-        onChange={(e) => handleOnFilled(e.target.value)}
-        onFilled={(e) => handleOnFilled(e)}
-      />
-      <Checkbox checked={checked} onCheck={(e) => handleOnCheck(e)} />
-    </>
-  );
-}
-type ProductSearchProps = { filled: string; onFilled: Function };
-function ProductSearch({ filled, onFilled }: ProductSearchProps) {
-  return (
-    <>
+    <div>
       <input
-        id="recherche-produit"
-        className="recherche-produit"
-        type="search"
-        value={filled}
-        onChange={(e) => onFilled(e.target.value)}
-      />{" "}
-    </>
-  );
-}
-function Checkbox({ checked, onCheck }) {
-  return (
-    <div className="produits-en-stock">
-      <input
-        id="produits-en-stock"
-        type="checkbox"
-        checked={checked}
-        onChange={(e) => onCheck(e.target.checked)}
+        placeholder="modifier le titre"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
-      <p>N'afficher que les produits en stocks ? </p>
+      <input
+        placeholder="modifier le nom"
+        value={firstname}
+        onChange={(e) => setFirstname(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder=""
+        value={timer}
+        min="10"
+        max="100"
+        id="timer"
+        onChange={(e) => setTimer(e.target.value)}
+      />
+      <p id="text-timer">Entrez un nombre plus grand que 0</p>
     </div>
-  );
-}
-
-function ProductTable({ products }: ProductRowProps[]) {
-  return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">Nom du Produit</th>
-            <th scope="col">Prix</th>
-          </tr>
-        </thead>
-        <tbody>
-          {CategoryOptions.map((_categorie, index) => (
-            <ProductCategory
-              key={index}
-              categorie={_categorie}
-              products={products}
-            />
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
-}
-function ProductCategory({ categorie, products }) {
-  return (
-    <>
-      <ProductCategoryRow categorie={categorie} />
-      {products
-        .filter((product) => product.categorie == categorie)
-        .map((product, index) => (
-          <ProductRow key={index} nom={product.nom} prix={product.prix} />
-        ))}
-    </>
-  );
-}
-
-function ProductCategoryRow({ categorie }: ProductCategoryProps) {
-  return (
-    <>
-      <tr className="categorie-produit">
-        <td>{categorie}</td>
-      </tr>
-    </>
-  );
-}
-
-function ProductRow({ nom, prix }: ProductRowProps) {
-  return (
-    <>
-      <tr className="produit">
-        <td>{nom}</td>
-        <td>{prix} €</td>
-      </tr>
-    </>
   );
 }
 
